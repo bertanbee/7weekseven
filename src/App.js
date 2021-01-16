b0523a39ac38e460ea96d52494254f7c0a465e14
Lucas Suzin Bertan <bertan1707@gmail.com>
2021-01-16 18:48:34 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Date from './components/Date/Date';
 import Logo from './components/Logo/Logo';
 import Body from './components/Body/Body';
+import Bottom from './components/Bottom/Bottom';
+import Cookies from './components/Cookies/Cookies';
 
 import './globalcss/global.css'
 import './globalcss/head.css';
@@ -9,7 +11,9 @@ import './globalcss/main-block.css';
 
 function App() {
   return (
+    <>
     <div className="main-block">
+      <Cookies></Cookies>
       <div className="head">
         <Date></Date>
         <Logo></Logo>
@@ -18,6 +22,8 @@ function App() {
         <Body></Body>
       </div>
     </div>
+    <Bottom></Bottom>
+    </>
   );
 }
 
